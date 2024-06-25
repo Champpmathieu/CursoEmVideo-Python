2efac2f37a672a9cb410a9124737730aad83aaf4
Eduardo Balbino Valença <valencabalbino@hotmail.com>
2024-06-25 18:51:54 -0300

    atualizacao

--- atalhos.py
+++ atalhos.py
@@ -5,3 +5,10 @@
 #Multi Seleção = Alt + Click
 #Zen Mode = Ctrl + K Z
 #IntelliSense = Ctrl + Espaço
+vitin = float(input('digite um numero: '))
+if vitin < 4:
+    print("dinonassauro")
+elif vitin >= 23:
+    print('rinocenonte')
+else:
+    print('maica')
