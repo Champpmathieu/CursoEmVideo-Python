703e0bc1dd12fa5453d845694d541897541fe5b9
Eduardo Balbino Valença <valencabalbino@hotmail.com>
2024-07-12 21:45:22 -0300

    new

--- ex59.py
+++ ex59.py
@@ -4,8 +4,10 @@
 #[ 3 ] maior
 #[ 4 ] novos números
 #[ 5 ] sair do programa
-from time import sleep
+import time
 soma = 0
+maior = 0
+multiplicacao = 0
 primeiroValor = float(input('Digite o primeiro valor: '))
 segundovalor = float(input("Digite o segundo valor: "))
 print('[ 1 ] somar')
@@ -27,3 +29,25 @@ while EscolhaDoUsuario not in [1, 2, 3, 4, 5]:
 if EscolhaDoUsuario == 1:
     soma = primeiroValor + segundovalor
     print('A soma do valor {} e do valor {} é {}!'.format(primeiroValor, segundovalor, soma))
+elif EscolhaDoUsuario == 2:
+    multiplicacao = primeiroValor * segundovalor
+    print('A multiplicação do valor {} e do valor {} é {}!')
+
+if EscolhaDoUsuario == 3:
+    #saber o maior valor das variaveis
+    if primeiroValor > segundovalor:
+        maior = primeiroValor
+    else:
+        maior = segundovalor
+print('O maior valor desses 2 numeros é {}'.format(maior))
+if EscolhaDoUsuario == 4:
+    primeiroValor = float(input('Digite o primeiro valor NOVO: '))
+    segundovalor = float(input("Digite o segundo valor NOVO: "))
+if EscolhaDoUsuario == 5:
+    print('Finalizando  .')
+    time.sleep(1)
+    print('Finalizando  ..')
+    time.sleep(1)
+    print('Finalizando ...')
+    time.sleep(1)
+    print('Programa finalizado com sucesso!')
